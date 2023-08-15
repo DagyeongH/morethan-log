5512ef1bd2d04baa6390e8f27986e55d5b31a9d3
Dagyeong <56528107+DagyeongH@users.noreply.github.com>
2023-08-15 12:57:07 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "dagyeong",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "data analyst",
+    bio: "hi !",
+    email: "hdk990425@gmail.com",
+    linkedin: "",
+    github: "DagyeongH",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: ``,
+      href: "",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "LOG (Life Of Gang-e)",
+    description: "welcome to my log!",
   },
 
   // CONFIG configration (required)
