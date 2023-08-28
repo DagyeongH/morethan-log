d965f2940c801100cc76ed3f725a26a6aad2efa9
Dagyeong <56528107+DagyeongH@users.noreply.github.com>
2023-08-29 00:55:11 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "dagyeong",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/apple-touch-icon.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "data analyst",
     bio: "hi !",
     email: "hdk990425@gmail.com",
